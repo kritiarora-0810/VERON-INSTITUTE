79f17624ab758948ad6c72a480cdaf0ad8af7c82
Kriti <kritiarora0810@gmail.com>
2025-02-08 21:10:45 +0530

    Changes updated and top college section added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import Carousel from '../components/carousell/carousell';
+import Carousel from '../components/carouselll/App';
 import Footer from '../components/Footer/Footer';
+import CollegeGrid from '../components/CollegeGrid/CollegeGrid';
 
 function HomePage() {
   return (
     <>
     <Navbar />
     <Carousel />
+    <CollegeGrid />
     <Footer />
       {/* <div className="h-screen w-screen bg-no-repeat bg-center bg-cover" style={{backgroundImage: "url(https://miro.medium.com/v2/resize:fit:1400/1*WoEu5XEHEwYFcDkbl1A3Bw.jpeg)"}}></div> */}
     </>
@@ -15,4 +17,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
